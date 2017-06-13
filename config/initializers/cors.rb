94cb6d8f0ab4bad2362aed8d038e85c3e0ceeaea
Rohit Moza <moza.rohit@gmail.com>
2017-06-13 21:45:22 +0000

    Fixing Cors2

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://dry-springs-90848.herokuapp.com:3000'
+    origins 'http://dry-springs-90848.herokuapp.com'
 
     resource '*',
       headers: :any,
