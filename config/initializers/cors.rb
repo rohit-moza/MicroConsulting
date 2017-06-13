368682c1c326428b984f5b6b319744f1abfe6f7c
Rohit Moza <moza.rohit@gmail.com>
2017-06-13 22:00:58 +0000

    Fixing Cors6

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://dry-springs-90848.herokuapp.com'
+    origins '*'
 
     resource '*',
       headers: :any,
